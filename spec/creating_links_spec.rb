9bd7edd8647eed1dc20b6b6984aa74e9a1543272
BartJudge <BartJudge@gmail.com>
2016-11-15 13:42:45 +0000

    setting up test and development environment

--- spec/creating_links_spec.rb
+++ spec/creating_links_spec.rb
@@ -4,8 +4,9 @@ feature 'Creating links' do
     visit '/links/new'
     fill_in 'url', with: 'http://www.zombo.com/'
     fill_in 'title', with: 'This is Zombocom'
+    click_button "Create link"
 
-    expect(current_path).to ew '/links'
+    expect(current_path).to eq '/links'
 
     within 'ul#links' do
       expect(page).to have_content('This is Zombocom')
