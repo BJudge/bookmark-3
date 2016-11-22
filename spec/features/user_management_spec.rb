7bd2d298df936f6c7bf4b689a10f22bb404cc914
BartJudge <BartJudge@gmail.com>
2016-11-22 14:50:04 +0000

    sessions route, authenticate user method

--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -32,5 +32,17 @@ feature 'User sign up' do
     expect {sign_up }.to_not change(User, :count)
     expect(page).to have_content('Email is already taken')
   end
+end
+
+feature 'User Sign in' do
+
+  let!(:user) do
+    User.create(email: 'john@example.com', password: 'secret1234', password_confirmation: 'secret1234')
+  end
+
+  scenario 'with correct credentials' do
+    sign_in(email: user.email, password: user.password)
+    expect(page).to have_content "Welcome, #{user.email}"
+  end
 
 end
--- spec/web_helper.rb
+++ spec/web_helper.rb
@@ -6,3 +6,11 @@ def sign_up(email: 'bart@example.com', password: '1234', password_confirmation: 
   fill_in :password_confirmation, with: password_confirmation
   click_button 'Sign up'
 end
+
+
+def sign_in(email:, password:)
+    visit '/sessions/new'
+    fill_in :email, with: email
+    fill_in :password, with: password
+    click_button 'Sign in'
+  end
